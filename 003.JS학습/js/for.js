5c8042b9aba6ac79d68c5e7625f35495d88ef1df
jingoochoi <jingoo0206@gmail.com>
2023-08-23 13:05:32 +0900

    Update for.js

--- 003.JS학습/js/for.js
+++ 003.JS학습/js/for.js
@@ -1,4 +1,10 @@
 // 3-3 external js
 // js외부파일은 html문서에 직접 들어가 동작하므로 utf 인코딩 설정이 불필요
-var mini=document.querySelector('.mini');
-console.log(mini);
+var mini=document.querySelectorAll('.mini');
+var bluebox=document.querySelector('.Bcase');
+// console.log(mini,bluebox);
+for(var i=0;i<mini.length;i++){mini[i].addEventListener('click',insertMini);}
+mini[0].addEventListener('click',insertMini);
+function insertMini(){
+    console.log('mini');
+}//insertmini
