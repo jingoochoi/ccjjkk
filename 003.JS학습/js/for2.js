6d18c44a767f2a98d163012437d12d43bb25b4b6
jingoochoi <jingoo0206@gmail.com>
2023-08-24 12:22:48 +0900

    Update for2.js

--- 003.JS학습/js/for2.js
+++ 003.JS학습/js/for2.js
@@ -1,1 +1,23 @@
 //for2 js
+/*          <li>
+                <img src="./images/dress/1.jpg" alt="dress">
+                <div class="iteminfo">
+                    <h3>name</h3>
+                    <h4>price</h4>
+                </div>
+            </li> */
+var listbox=document.querySelector('.listbox');
+// console.log(listbox);
+var hcode='';
+hcode+='<ul>';
+for(var i=0;i<30;i++){hcode+=`
+<li>
+<img src="./images/dress/${i+1}.jpg" alt="dress">
+<div class="iteminfo">
+    <h3>name ${i+1}</h3>
+    <h4>price ${i+1}</h4>
+</div>
+</li>
+`}
+hcode+='</ul>';
+listbox.innerHTML=hcode
