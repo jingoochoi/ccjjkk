dd91ef032365ca8e4aa5c3f96de720bd30e0c929
jingoochoi <jingoo0206@gmail.com>
2023-08-24 11:01:54 +0900

    Update for.js

--- 003.JS학습/js/for.js
+++ 003.JS학습/js/for.js
@@ -3,9 +3,11 @@
 var mini=document.querySelectorAll('.mini');
 var bluebox=document.querySelector('.minispace');
 var cntspan=document.querySelector('.stxt span');
+var btnreset=document.querySelector('.rbtn');
 // console.log(mini,bluebox);
 for(var i=0;i<mini.length;i++){mini[i].addEventListener('click',insertMini);}
 // console.log(i);
+btnreset.addEventListener('click',reset);
 function insertMini(){
     // console.log('mini',this);
     var cnt=this.getAttribute('data-cnt');
@@ -16,3 +18,8 @@ function insertMini(){
     cntspan.innerHTML=countmini;
     // console.log(typeof countmini);//typeof=number,string,null...
 }//insertmini
+function reset(){
+    // console.log('reset');
+    bluebox.innerHTML='';
+    cntspan.innerHTML='0';
+}//reset
