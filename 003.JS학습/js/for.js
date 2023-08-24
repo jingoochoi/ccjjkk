7514b7332ec7892a358db9b4ca2f5a262f6e6a4d
jingoochoi <jingoo0206@gmail.com>
2023-08-24 10:48:55 +0900

    Update for.js

--- 003.JS학습/js/for.js
+++ 003.JS학습/js/for.js
@@ -2,13 +2,17 @@
 // js외부파일은 html문서에 직접 들어가 동작하므로 utf 인코딩 설정이 불필요
 var mini=document.querySelectorAll('.mini');
 var bluebox=document.querySelector('.minispace');
+var cntspan=document.querySelector('.stxt span');
 // console.log(mini,bluebox);
 for(var i=0;i<mini.length;i++){mini[i].addEventListener('click',insertMini);}
 // console.log(i);
 function insertMini(){
     // console.log('mini',this);
     var cnt=this.getAttribute('data-cnt');
-    console.log(cnt);
+    // console.log(cnt);
     for(var j=0;j<cnt;j++){bluebox.innerHTML+=`<img src="./images/Minions.png" alt="미니언즈" class="mini" title="나를 클릭하면 열개씩 가방에 들어갈꼬얌~!" data-cnt="10" />`;}
-    
+    var countmini=bluebox.querySelectorAll('img').length;
+    // console.log(countmini);
+    cntspan.innerHTML=countmini;
+    // console.log(typeof countmini);//typeof=number,string,null...
 }//insertmini
