365598ecc1864f240defc806f31d89d40009cbfa
jingoochoi <jingoo0206@gmail.com>
2023-08-30 15:21:27 +0900

    .

---
+++ 03.Robot_PJ/js/avengers.js
@@ -0,0 +1,9 @@
+// console.log(character)
+const qs=(x)=>document.querySelector(x)
+const qa=(x)=>document.querySelectorAll(x)
+const avengers=qs('.avengersbox')
+// console.log(avengers)
+let hcode=''
+for(let x in character){
+    console.log(x)
+}
