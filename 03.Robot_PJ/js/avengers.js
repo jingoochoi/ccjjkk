f9024e6c9504bc03b37a384502547cf32ddfb530
jingoochoi <jingoo0206@gmail.com>
2023-08-30 15:55:03 +0900

    .

--- 03.Robot_PJ/js/avengers.js
+++ 03.Robot_PJ/js/avengers.js
@@ -5,5 +5,34 @@ const avengers=qs('.avengersbox')
 // console.log(avengers)
 let hcode=''
 for(let x in character){
-    console.log(x)
+    // console.log(x)
+    hcode+=`
+    <div class="hero">
+    <!-- 이미지 -->
+    <img src="./ab_img/${character[x]['이미지명']}.png" alt="${x}">
+    <!-- 소개글박스 -->
+    <article class="txt">
+        <div>
+            <h3>${x}</h3>
+            <p>${character[x]['설명']}</p>
+        </div>
+    </article>
+</div>
+    `
 }
+// console.log(hcode)
+ /************************************************* 
+                [ 객체를 위한 for in 문 ]
+
+                - 구문: 
+                for(변수 in 객체){코드}
+
+                - 작동원리:
+                객체의 개수만큼 순서대로 속성명과 속성값을 가져옴
+
+                - 변수는 객체의 속성명이다!
+
+                - 객체의 값을 사용하려면 for in 문 안에
+                    객체[변수] 로 쓰면됨!
+
+            *************************************************/
