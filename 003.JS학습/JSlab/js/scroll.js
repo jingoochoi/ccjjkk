2e18be8b5b13a2b7e0c03cfa7c991ef12c49e696
jingoochoi <jingoo0206@gmail.com>
2023-08-31 12:43:53 +0900

    Update scroll.js

--- 003.JS학습/JSlab/js/scroll.js
+++ 003.JS학습/JSlab/js/scroll.js
@@ -2,9 +2,12 @@ let bgnum=0;
 window.addEventListener('wheel',wlfn)
 function wlfn(e) {
     // console.log('f')
-    let delta=e.wheelDelta
+    let delta=e.wheelDelta//wheel direction(-=down,+=up)
     // console.log(delta)
     if(delta<0)bgnum++
     else bgnum--
+    if(bgnum<0)bgnum=0
+    if(bgnum>6)bgnum=6
+    // console.log(bgnum)
     window.scrollTo(0,window.innerHeight*bgnum)
 }
