89bd7fb6e3ad1f3756fae5513e29203fd3f6c588
jingoochoi <jingoo0206@gmail.com>
2023-09-01 16:16:54 +0900

    .

--- dkb/js/main.js
+++ dkb/js/main.js
@@ -3,4 +3,6 @@ window.addEventListener('DOMContentLoaded',ldfn)
 function ldfn() {
     // console.log('b')
     startSS()
+    let dcbx=document.querySelectorAll('.descbox')
+    dcbx.forEach(e=>{e.stopPropagation()})//stop the event bubbling of event obj
 }
