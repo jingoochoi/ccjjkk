2f1e1feef7578be6f2f35787de43b6522a03c36b
jingoochoi <jingoo0206@gmail.com>
2023-09-01 11:51:56 +0900

    .

---
+++ test/05.3DAni/js/cube2.js
@@ -0,0 +1,28 @@
+// 큐브 애니 JS //////////////
+
+/************************************* 
+    [구현내용]
+    - "돌아!"버튼을 클릭하면 멈춰있던
+    큐브가 돌아감. 이때 버튼이 "멈춰!"
+    버튼으로 변경되어 있음!
+    - "멈춰!" 버튼을 클릭하면 돌고있던
+    큐브가 멈춤. 이때 버튼이 "돌아!"
+    버튼으로 변경되어 있음!
+
+*************************************/
+window.addEventListener('DOMContentLoaded',clft)
+const qs=x=>document.querySelector(x)
+const qa=x=>document.querySelectorAll(x)
+function clft() {
+    // console.log('b')
+    const btngo=qs('.btngo')
+    const cube=qs('.cube')
+    // console.log(btngo,cube)
+    btngo.onclick=(e)=>{
+        // console.log('b')
+        cube.classList.toggle('on')
+        let btxt=e.currentTarget.innerHTML
+        // console.log(btxt)
+        e.currentTarget.innerHTML=btxt==='ROLL'?'STOP!':'ROLL'
+    }
+}
