c719e952d7831cfc33b7142bd36652e044f4cee1
jingoochoi <jingoo0206@gmail.com>
2023-09-04 10:25:02 +0900

    .

--- 003.JS학습/JSlab/js/following.js
+++ 003.JS학습/JSlab/js/following.js
@@ -2,5 +2,18 @@ window.addEventListener('DOMContentLoaded',ldft)
 const qs=x=>document.querySelector(x)
 const qa=x=>document.querySelectorAll(x)
 function ldft() {
-    console.log('b')
+    // console.log('b')
+    const contbox=qs('.contbox')
+    // console.log(contbox)
+    let hcode=''
+    for(let i=1;i<=50;i++){
+        hcode+=`
+        <div>
+        <img src="../images/dress/${i}.jpg" alt="dress">
+        <a href="#" class="link">your dress</a>
+        </div>
+        `
+    }
+    // console.log(hcode)
+    contbox.innerHTML=hcode
 }
