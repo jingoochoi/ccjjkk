9f21954d3f641dcb257856b0f4ffa40349d1ab5f
jingoochoi <jingoo0206@gmail.com>
2023-09-08 11:20:56 +0900

    Update main.js

--- 06.CGV_PJ/003.구현소스/js/main.js
+++ 06.CGV_PJ/003.구현소스/js/main.js
@@ -7,3 +7,19 @@ const domft={
      qsa : (x) => document.querySelectorAll(x),
      addevt:(a,b,c)=>a.addEventListener(b,c),
 }
+const postermn=domft.qsa('.postermn a')
+const screen=domft.qs('.screen')
+// console.log(postermn,screen)
+const mvcode={
+    '닥터스트레인지2':'mI9oyFMUlfg',
+    '쥬라기월드:도미니언':'DSEfRVqjbFA',
+    '브로커':'DpVAb7Bi5UQ',
+    '범죄도시2':'aw9j_23nORs',
+    '몬스터싱어':'ELuQ4sMp4yw',
+    '스파이더맨:노웨이홈':'W7edvITC9g4',
+}
+postermn.forEach(a=>{
+    domft.addevt(a,'click',function () {
+        // console.log('b',this)
+    })
+})
