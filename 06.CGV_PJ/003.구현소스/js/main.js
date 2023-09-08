42c3b6d4d2f35f327e961661d8b5219fdb169eb9
jingoochoi <jingoo0206@gmail.com>
2023-09-08 11:03:37 +0900

    .

--- 06.CGV_PJ/003.구현소스/js/main.js
+++ 06.CGV_PJ/003.구현소스/js/main.js
@@ -1,5 +1,9 @@
 // CGV PJ 메인 페이지 JS - main.js
 
-// 요소선택함수 ////////
-const qs = (x) => document.querySelector(x);
-const qsa = (x) => document.querySelectorAll(x);
+// DOM function obj
+const domft={
+    // 요소선택함수 ////////
+     qs : (x) => document.querySelector(x),
+     qsa : (x) => document.querySelectorAll(x),
+     addevt:(a,b,c)=>a.addEventListener(b,c),
+}
