741a7bd0d874c6568d0e2802ebff14bb82e665dc
jingoochoi <jingoo0206@gmail.com>
2023-09-12 12:12:13 +0900

    .

---
+++ 05.Nav_PJ/js/nav06.js
@@ -0,0 +1,11 @@
+const domFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+  
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  }; /////// domFn 객체 /////////////
+  
