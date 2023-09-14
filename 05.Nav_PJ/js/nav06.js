a4236200b99ff088bc4080e017486895ad5e689d
jingoochoi <jingoo0206@gmail.com>
2023-09-14 10:07:49 +0900

    Update nav06.js

--- 05.Nav_PJ/js/nav06.js
+++ 05.Nav_PJ/js/nav06.js
@@ -8,6 +8,7 @@ const domFn = {
     // 이벤트셋팅함수
     addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
   }; /////// domFn 객체 /////////////
+//   console.log("red","color:red;")
   const gnbbox=domFn.qs('.gnb')
   let hcode=''
   for (let x in mdata) {
@@ -15,16 +16,16 @@ const domFn = {
     `
     <ul>
         <li>
-            <a href="">${x}</a>
+            <a href="">1차</a>
             <div class="smenu">
                 <aside class="smbx">
                     <h2>
-                        <div class="stit">2차</div>
+                        <div class="stit">1차</div>
                         <a href="">전체보기</a>
                         <div class="swrap">
                             <dl>
-                                <dt></dt>
-                                <dd><a hrefa></dd>
+                                <dt>2차</dt>
+                                <dd><a href="">3차</dd>
                             </dl>
                         </div>
                     </h2>
