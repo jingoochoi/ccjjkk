39789f1de0e54c8aa3b86ac0a6d30dbd51579069
jingoochoi <jingoo0206@gmail.com>
2023-09-14 16:12:51 +0900

    .

--- 003.JS학습/JSlab/js/dance.js
+++ 003.JS학습/JSlab/js/dance.js
@@ -10,7 +10,7 @@ const domFn = {
     addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
     bcr:a=>a.getBoundingClientRect().top
   }; /////// domFn 객체 /////////////
-  console.log("%c💃let's dance🕺","color: red; background-image: linear-gradient(to bottom right,red,orange,yellow,green,blue,darkblue,violet); line-height: 2; padding: 30px; font-size: 30px")
+  console.log("%c💃let's dance🕺","color: hotpink; background-image: linear-gradient(to bottom right,red,orange,yellow,green,blue,darkblue,violet); line-height: 2; padding: 30px; font-size: 30px")
   const slidepg=domFn.qs('.slidePg')
   // console.log(slidepg)
   let hcode=''
@@ -23,10 +23,32 @@ const domFn = {
   hcode=`<ul>${hcode}</ul>`
   slidepg.innerHTML=hcode
   domFn.addEvt(window,'scroll',scrft)
-  
+  const tpg=domFn.qs('.tpg')
+  // let num=0
+  const target=domFn.qs('.slidePg>ul')
   function scrft() {
-    // console.log(domFn.bcr(slidepg))
-    if (domFn.bcr(slidepg)==0) {
-      
+    // console.log(domFn.bcr(tpg))
+    if (domFn.bcr(tpg)<=0&&domFn.bcr(tpg)>-3000) {
+      // num++
+      // window.scrollTo(window.scrollX,0)
+      target.style.left=domFn.bcr(tpg)+'px'
+    }
+    else if (domFn.bcr(target)>0) {
+      target.style.left=0
     }
   }
+  const gnbli=domFn.qsa('.gnb>ul>li')
+  const mbg=domFn.qs('.mbg')
+  gnbli.forEach(a=>{
+
+    domFn.addEvt(a,'mouseover',ovft)
+    domFn.addEvt(a,'mouseout',outft)
+    function ovft() {
+      let posl=this.offsetLeft
+      mbg.style.left=posl+'px'
+      mbg.style.opacity=1
+    }
+    function outft() {
+      mbg.style.opacity=0
+    }
+  })
