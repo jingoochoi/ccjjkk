2f550b0d02912c393123c56826c5c9e7ab6dc5a7
jingoochoi <jingoo0206@gmail.com>
2023-09-14 11:07:34 +0900

    Update nav06.js

--- 05.Nav_PJ/js/nav06.js
+++ 05.Nav_PJ/js/nav06.js
@@ -8,7 +8,7 @@ const domFn = {
     // 이벤트셋팅함수
     addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
   }; /////// domFn 객체 /////////////
-  console.log("%cwelcome to chicor", "background-color: pink; color: black; padding : 20px; font-size: 50px")
+  console.log("%cwelcome to chicor", "background-image: linear-gradient(to bottom right,red,orange,yellow,green,blue,darkblue,violet); color: black; padding : 20px; font-size: 50px")
   const gnbbox=domFn.qs('.gnb')
   let hcode=''
   for (let x in mdata) {
@@ -46,6 +46,7 @@ const domFn = {
     return hcode
   }
 //   배열 데이터를 바꿔서 다시 배열로 만들기->.map((a,b,c)=>{code})
+// 새로 만들어진 데이터를 현재 자리에 그대로 출력코자 할 때 배열메서드 join('sth')을 이용하여 배열을 하나의 문자형 데이터로 만들어주면 편리하다
 // const aa=['','',''];+씨
 // const bb=aa.map(a=>a+'씨')->[' 씨',' 씨',' 씨']
   console.log(hcode)
