3dcfa14c198b2c9d86ca201394bd9157516e5344
jingoochoi <jingoo0206@gmail.com>
2023-09-15 13:17:53 +0900

    .

--- 003.JS학습/JSlab/js/cube2.js
+++ 003.JS학습/JSlab/js/cube2.js
@@ -2,28 +2,42 @@
 
 /************************************* 
     [구현내용]
-    - "돌아!"버튼을 클릭하면 멈춰있던
-    큐브가 돌아감. 이때 버튼이 "멈춰!"
-    버튼으로 변경되어 있음!
-    - "멈춰!" 버튼을 클릭하면 돌고있던
-    큐브가 멈춤. 이때 버튼이 "돌아!"
-    버튼으로 변경되어 있음!
+    - 휠에 따라 기본기능은 막고 큐브를 회전하는 속성인 로테와이를 이용한 큐브회전적용
+    
 
 *************************************/
-// window.addEventListener('DOMContentLoaded',clft)
-// const qs=x=>document.querySelector(x)
-// const qa=x=>document.querySelectorAll(x)
-// function clft() {
-//     // startSS()
-//     // console.log('b')
-//     const btngo=qs('.btngo')
-//     const cube=qs('.cube')
-//     // console.log(btngo,cube)
-//     btngo.onclick=(e)=>{
-//         // console.log('b')
-//         cube.classList.toggle('on')
-//         let btxt=e.currentTarget.innerHTML
-//         // console.log(btxt)
-//         e.currentTarget.innerHTML=btxt==='ROLL'?'STOP!':'ROLL'
-//     }
-// }
+// DOM 함수 객체 //////////////
+const domFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+  
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  }; /////// domFn 객체 /////////////
+const deg=40
+let sts=0
+let num=0
+const time=300
+const cube=domFn.qs('.cube')
+domFn.addEvt(window,'wheel',wft)
+function wft() {
+    if (sts==1) {
+        return sts
+    }
+    sts=1
+    setTimeout(() => {
+        sts=0
+    }, time);
+    // console.log('b')
+    let dt=event.wheelDelta
+    if (dt<0) {
+        num--
+    }
+    if (dt>0) {
+        num++
+    }
+    cube.style.transform=`rotateY(${deg*num}deg)`
+}
