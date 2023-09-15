3a7045d4923f5a34584c763ef13c9973ba1c7519
jingoochoi <jingoo0206@gmail.com>
2023-09-15 12:35:04 +0900

    .

--- 003.JS학습/JSlab/js/cube2.js
+++ 003.JS학습/JSlab/js/cube2.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded',clft)
 const qs=x=>document.querySelector(x)
 const qa=x=>document.querySelectorAll(x)
 function clft() {
-    startSS()
+    // startSS()
     // console.log('b')
     const btngo=qs('.btngo')
     const cube=qs('.cube')
