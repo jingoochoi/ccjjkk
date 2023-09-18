4359dfa158b4a363b41e39046bf1fd04072724c7
jingoochoi <jingoo0206@gmail.com>
2023-09-18 11:20:23 +0900

    .

--- 06.CGV_PJ/003.구현소스/js/intro.js
+++ 06.CGV_PJ/003.구현소스/js/intro.js
@@ -1,2 +1,10 @@
 // 인트로 페이지 JS - intro.js
-
+const myvid=document.querySelector('#myvid')
+// timeupdate event
+myvid.addEventListener('timeupdate',function () {
+    // console.log('b')
+    let stop=myvid.paused//pause=true
+    if (stop) {
+        location.href='main.html'
+    }
+})
