3118899261348583c7532eaad2afd3c1d5ff723b
jingoochoi <jingoo0206@gmail.com>
2023-09-20 11:26:32 +0900

    Update main.js

--- 10.Ossomae_PJ/js/main.js
+++ 10.Ossomae_PJ/js/main.js
@@ -8,8 +8,52 @@ abtn.forEach(a=>{
 })
 function clft() {
     // console.log(this)
+    let sts=0
+    if (sts==1) {
+        return sts
+    }
+    sts=1
+    setTimeout(() => {
+        sts=0
+    }, 3000);
     let rb=this.classList.contains('rb')
     if (rb) {
         gbx.appendChild(dom.qsaEl(gbx,'div')[0])
     }else gbx.insertBefore(dom.qsaEl(gbx,'div')[dom.qsaEl(gbx,'div').length-1],dom.qsaEl(gbx,'div')[0])
 }
+dom.addEvt(document,'keydown',kdft)
+function kdft() {
+    let sts=0
+    if (sts==1) {
+        return sts
+    }
+    sts=1
+    setTimeout(() => {
+        sts=0
+    }, 3000);
+    if (event.key=='ArrowRight') {
+        gbx.appendChild(dom.qsaEl(gbx,'div')[0])
+    }
+    if (event.key=='ArrowLeft') {
+        gbx.insertBefore(dom.qsaEl(gbx,'div')[dom.qsaEl(gbx,'div').length-1],dom.qsaEl(gbx,'div')[0])
+    }
+}
+// setInterval(() => {
+//     gbx.appendChild(dom.qsaEl(gbx,'div')[0])
+// }, 3000);
+const go=function () {
+    gbx.appendChild(dom.qsaEl(gbx,'div')[0])
+}
+let autoi,autot
+const auto=function () {
+    autoi=setInterval(go,3000)
+}
+auto()
+const clear=function () {
+    clearInterval(autoi)
+    clearTimeout(autot)
+    autot=setTimeout(auto,10000)
+}
+clear()
+abtn.forEach(a=>dom.addEvt(a,'click',clear))
+dom.addEvt(document,'keydown',clear)
