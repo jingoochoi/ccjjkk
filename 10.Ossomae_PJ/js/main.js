ac6d866ff5e39dc9193ada58facf912596865973
jingoochoi <jingoo0206@gmail.com>
2023-09-20 11:04:40 +0900

    .

--- 10.Ossomae_PJ/js/main.js
+++ 10.Ossomae_PJ/js/main.js
@@ -1,1 +1,15 @@
 // 옷소매 갤러리 JS - main.js
+import dom from "./dom.js";
+console.log("%c🤴🧡","background-color:pink;line-height:2;padding:30px;font-size:30px")
+const abtn=dom.qsa('.abtn')
+const gbx=dom.qs('.gbx')
+abtn.forEach(a=>{
+    dom.addEvt(a,'click',clft)
+})
+function clft() {
+    // console.log(this)
+    let rb=this.classList.contains('rb')
+    if (rb) {
+        gbx.appendChild(dom.qsaEl(gbx,'div')[0])
+    }else gbx.insertBefore(dom.qsaEl(gbx,'div')[dom.qsaEl(gbx,'div').length-1],dom.qsaEl(gbx,'div')[0])
+}
