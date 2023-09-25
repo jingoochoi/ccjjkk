130b9e2bb60746316d78ce79ddfb1a74eb57e65c
jingoochoi <jingoo0206@gmail.com>
2023-09-25 12:21:11 +0900

    .

---
+++ 003.JS학습/JSlab/js/bannerstyle.js
@@ -0,0 +1,157 @@
+// 쇼핑몰 배너 JS - 01.가로방향 배너 슬라이드 //
+
+// DOM 선택함수
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
+const qsael=(a,b)=>a.querySelectorAll(b)
+// addEvent 함수
+// ele - 요소, evt - 이벤트, fn - 함수
+const addEvt = 
+(ele, evt, fn) => ele.addEventListener(evt, fn);
+
+// HTML태그 로딩후 loadFn함수 호출! ///
+addEvt(window,"DOMContentLoaded", slideft);
+
+/***************************************************** 
+    [ 슬라이드 이동 기능정의 ]
+    1. 이벤트 종류: click
+    2. 이벤트 대상: 이동버튼(.abtn)
+    3. 변경 대상: 슬라이드 박스(#slide)
+    4. 기능 설계:
+
+        (1) 오른쪽 버튼 클릭시 다음 슬라이드가
+            나타나도록 슬라이드 박스의 left값을
+            -100%로 변경시킨다.
+            -> 슬라이드 이동후!!! 
+            바깥에 나가있는 첫번째 슬라이드
+            li를 잘라서 맨뒤로 보낸다!
+            동시에 left값을 0으로 변경한다!
+
+        (2) 왼쪽버튼 클릭시 이전 슬라이드가
+            나타나도록 하기위해 우선 맨뒤 li를
+            맨앞으로 이동하고 동시에 left값을
+            -100%로 변경한다.
+            그 후 left값을 0으로 애니메이션하여
+            슬라이드가 왼쪽에서 들어온다.
+
+        (3) 공통기능: 슬라이드 위치표시 블릿
+            - 블릿 대상: .indic li
+            - 변경 내용: 슬라이드 순번과 같은 순번의
+            li에 클래스 "on"주기(나머진 빼기->초기화!)
+
+*****************************************************/
+let clicksts=0
+const TIME=300
+/****************************************** 
+    함수명: loadFn
+    기능: 로딩 후 버튼 이벤트 및 기능구현
+******************************************/
+function slideft(a) {
+    // console.log("로딩완료!");
+    const slide=qs(a)
+    const abtn=qsael(a,'.abtn')
+    const indic=qsael(a,'.indic li')
+    
+    // data-=user setting attribute
+    slide.querySelectorAll('li').forEach((a,idx)=>a.setAttribute('data-seq',idx))
+    abtn.forEach(a=>{addEvt(a,'click',gs)})
+
+    function gs() {
+        // console.log(this)
+        // classList.contains(classname)=boolean whether contains classname
+        if (clicksts==1) {
+            return
+        }
+        clicksts=1
+        setTimeout(() => {
+            clicksts=0
+        }, TIME);
+        let isr=this.classList.contains('ab2')
+        if (this.classList.contains('ab2')) {
+            rightsl()
+        }
+        if (this.classList.contains('ab1')) {            
+            slide.insertBefore(slide.querySelectorAll('li')[4],slide.querySelectorAll('li')[0])
+            slide.style.left='-330%'
+            slide.style.transition='none'
+            setTimeout(() => {
+                slide.style.left='-220%'
+                slide.style.transition=TIME+'ms ease-in-out'
+            }, 0);
+        }
+        chgid(isr)
+        clearauto()
+    }
+    
+    function chgid(isr) {
+        let nowsq=slide.querySelectorAll('li')[isr?1:0].getAttribute('data-seq')
+        indic.forEach(function (a,idx) {
+            if (idx==nowsq) {
+                a.classList.add('on')
+            }else a.classList.remove('on')
+        })
+    }
+    function rightsl() {
+        slide.style.left='-330%'
+        slide.style.transition=TIME+'ms ease-in-out'
+        setTimeout(() => {
+            slide.appendChild(slide.querySelectorAll('li')[0])
+            slide.style.left='-220%'
+            slide.style.transition='none'
+        }, TIME);
+    }
+    addEvt(document,'keydown',kdft)
+    function kdft() {
+        // console.log('b')
+        if (clicksts==1) {
+            return
+        }
+        clicksts=1
+        setTimeout(() => {
+            clicksts=0
+        }, 300);
+        if (event.key=='ArrowRight') {
+            slide.style.left='-330%'
+            slide.style.transition='.3s ease-in-out'
+            setTimeout(() => {
+                slide.appendChild(slide.querySelectorAll('li')[0])
+                slide.style.left='-220%'
+                slide.style.transition='none'
+            }, 3-220%0);
+        }
+        if (event.key=='ArrowLeft') {
+            slide.insertBefore(slide.querySelectorAll('li')[4],slide.querySelectorAll('li')[0])
+            slide.style.left='-330%'
+            slide.style.transition='none'
+            setTimeout(() => {
+                slide.style.left='-220%'
+                slide.style.transition='.3s ease-in-out'
+            }, 0);
+        }
+        let nowsq=slide.querySelectorAll('li')[event.key=='ArrowRight'?1:0].getAttribute('data-seq')
+        indic.forEach(function (a,idx) {
+            if (idx==nowsq) {
+                a.classList.add('on')
+            }else a.classList.remove('on')
+        })
+    }
+    let autoI;
+    let autot
+    function slideauto() {
+        autoI= setInterval(() => {
+            // abtn[1].click()
+            rightsl()
+            chgid(1)
+        }, 3000);
+        
+    }
+    slideauto()
+    function clearauto() {
+        clearInterval(autoI)
+        clearTimeout(autot)//stop timeout overriding(gwang cl gum ji)
+        autot= setTimeout(() => {
+            slideauto()
+        }, 10000);
+    }
+} //////////////// loadFn 함수 ///////////////
+/////////////////////////////////////////////
