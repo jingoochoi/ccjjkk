2381ca148eedb74eefca05e1e0c32e15eda55c80
jingoochoi <jingoo0206@gmail.com>
2023-10-06 17:20:27 +0900

    .

---
+++ 004.React학습/react-cdn/js/avengers.jsx
@@ -0,0 +1,12 @@
+function Avengers() {
+    return(
+        <React.Fragment>
+            <h1>Avengers!!!</h1>
+            <img src="./images/ab1.jpg" alt="avengers" />
+            <img src="./images/ab2.jpg" alt="avengers" />
+            <img src="./images/ab3.jpg" alt="avengers" />
+            <img src="./images/ab4.jpg" alt="avengers" />
+        </React.Fragment>
+    )
+}
+export default Avengers
