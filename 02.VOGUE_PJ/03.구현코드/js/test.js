12c6dfcec00b169987005a9df18ae2d842be6ba2
jingoochoi <jingoo0206@gmail.com>
2023-10-10 17:10:34 +0900

    .

--- 02.VOGUE_PJ/03.구현코드/js/test.js
+++ 02.VOGUE_PJ/03.구현코드/js/test.js
@@ -1,2 +1,8 @@
 // 테스트 JS
 console.log("%c　👩\n  👗\n","font-size:100px;text-align:center")
+import aespa from './com.js'
+import stayc from './dom.js'
+const top=stayc.qs('.toparea')
+top.innerHTML=aespa.toparea
+const foot=stayc.qs('.footarea')
+foot.innerHTML=aespa.footarea
