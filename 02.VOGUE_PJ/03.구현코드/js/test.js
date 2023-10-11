d3a5eba7471e3dfa9e0efbdf58caecfdac999e87
jingoochoi <jingoo0206@gmail.com>
2023-10-11 17:11:00 +0900

    .

--- 02.VOGUE_PJ/03.구현코드/js/test.js
+++ 02.VOGUE_PJ/03.구현코드/js/test.js
@@ -2,6 +2,10 @@
 console.log("%c　👩\n  👗\n","font-size:100px;text-align:center")
 import aespa from './com.js'
 import stayc from './dom.js'
+// 부드러운 스크롤 모듈
+import startSS from "./smoothScroll23.js";
+// 부드러운 스크롤 적용 //////////
+startSS();
 const top=stayc.qs('.toparea')
 top.innerHTML=aespa.toparea
 const foot=stayc.qs('.footarea')
