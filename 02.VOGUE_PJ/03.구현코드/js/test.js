f1d9c7a3b67b1a7a22d572f8cf286a2ede566e25
jingoochoi <jingoo0206@gmail.com>
2023-10-11 17:33:01 +0900

    Update test.js

--- 02.VOGUE_PJ/03.구현코드/js/test.js
+++ 02.VOGUE_PJ/03.구현코드/js/test.js
@@ -1,5 +1,7 @@
 // 테스트 JS
 console.log("%c　👩\n  👗\n","font-size:100px;text-align:center")
+console.log("%cd","font-size:100px;background-image:linear-gradient(to bottom,blue,blue 50%,yellow 51%,yellow);padding-left:100px;padding-right:100px;line-height:1.5;color:transparent",'pray for Ukraine')
+// console.log("%c●","font-size:130px;color:red;padding-left:100px;padding-right:100px;border:1px solid black;line-height:1.5;")
 import aespa from './com.js'
 import stayc from './dom.js'
 // 부드러운 스크롤 모듈
