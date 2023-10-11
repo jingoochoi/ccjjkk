76c32c3b03cf7cad82c105d04386154deae1bd79
jingoochoi <jingoo0206@gmail.com>
2023-10-11 16:20:19 +0900

    Update 06.Event.jsx

--- 004.React학습/react-cdn/js/06.Event.jsx
+++ 004.React학습/react-cdn/js/06.Event.jsx
@@ -37,13 +37,55 @@ function Event() {
             background-color: rgba(0,0,0,.5);
         `
         }, 30);
+        setTimeout(() => {
+            document.querySelectorAll('button')[0].style.display='inline-block'
+        }, 3000);
+        ReactDOM.render(<Images src={lamp} alt="aladin"/>,document.querySelector('#ala'))
+    }
+    const getLamp= function () {
+        let lamp=document.querySelector('.lamp')
+        ReactDOM.render(<Images src="https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/3168457870/B.png" alt="lamp"/>,lamp)
+        lamp.querySelector('img').style.cssText=`
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 200px;
+        border-radius: 50%;
+        transition: 2s;
+        `
+        setTimeout(() => {
+            lamp.querySelector('img').style.cssText=`
+            position: absolute;
+            top: 310px;
+            right: calc(50% - 100px);
+            width: 200px;
+            border-radius: 50%;
+            transition: 2s,right 2s 2s;
+            `
+            // 
+        }, 300);
+        setTimeout(() => {
+            document.querySelectorAll('button')[1].style.display='inline-block'
+        }, 3000);
+    }
+    const getFerrari=function () {
+        ReactDOM.render(<Images src="https://www.pngplay.com/wp-content/uploads/13/Ferrari-458-Transparent-PNG.png" alt="ferrari"/>,document.querySelector('#ferrari'))
+        document.querySelector('#ferrari img').style.title='drive'
     }
     return(
         <React.Fragment>
             <div id="tbox" style={{textAlign:'center'}}>
                 <img src="https://images.chosun.com/resizer/SFIqPKffr3HQHoHFOxKvnN-L2YU=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/NVMCI5M33KHBCY4JVHDPWRCBYY.jpg" alt="genie" onMouseOver={()=>{aladin('./images/ala4.jpg')}} />
             </div>
+            <div className="lamp"></div>
+            <button onClick={getLamp}>bring the lamp</button>
+            <br /><button onClick={getFerrari}>show me the ferrari</button>
         </React.Fragment>
+    )
+}
+function Images(p) {
+    return(
+        <img src={p.src} alt={p.alt} />
     )
 }
 ReactDOM.render(<Event/>,document.querySelector('#root'))
