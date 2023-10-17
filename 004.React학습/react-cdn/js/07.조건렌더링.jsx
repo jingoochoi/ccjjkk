0d3b10fea2343b56e84a3f47d8c21b5c07de37a1
jingoochoi <jingoo0206@gmail.com>
2023-10-17 13:18:55 +0900

    Update 07.조건렌더링.jsx

--- 004.React학습/react-cdn/js/07.조건렌더링.jsx
+++ 004.React학습/react-cdn/js/07.조건렌더링.jsx
@@ -100,3 +100,78 @@ function Mvcl(p) {
     )
 }
 ReactDOM.render(<Mvcl list={movs}/>,document.querySelector('#root4'))
+/********************************************************** 
+    조건 연산자(삼항연산자)를 사용하여 조건부 랜더링하기 hook
+**********************************************************/
+// 명화 데이터
+const worksrc = {
+    "피카소":"https://m.theartin.net/web/product/big/201907/30c5a0fdd153bfdfdc8f19b2f4166fa8.jpg",
+    "모네":"https://dimg.donga.com/wps/NEWS/IMAGE/2015/12/11/75316598.3.jpg"
+};
+function Work(p) {
+    return(
+        <div>
+            <h2>{p.pt}</h2>
+            <img src={worksrc[p.pt]} alt={p.name} style={{width:'300px'}} title={p.name} />
+        </div>
+    )
+}
+function Arts(p) {
+    // let sult=p.isChange Hook->
+    const [sult,setSult]=React.useState(p.isChange)
+    const gain=function () {
+        // console.log('b')
+        // setSult=!sult Hook->
+        setSult(!sult)
+        
+    }
+    return(
+        <React.Fragment>
+            <Tits title='명화'/>
+            <button onClick={gain} style={{cursor:'pointer'}}>go to change</button>
+            {sult?<Work pt='피카소' name='우는여인'/>:<Work pt='모네' name='양산을쓴여인'/>}
+        </React.Fragment>
+    )
+}
+ReactDOM.render(<Arts isChange={true}/>,document.querySelector('#root5'))
+
+/********************************************************* 
+    [ 리액트 훅크 : React Hook ]
+    - 일반적으로 리액트에 사용되는 변수는 처음에 
+    컴포넌트에 전달되어 초기 셋팅에 활용된다.
+    그런데 이 변수가 변경될 경우 컴포넌트의 변경이
+    자동적으로 이루어지지 않는다!
+    이런 종류의 변수 업데이트가 가상돔과 실제돔에
+    바로 반영되도록 실시간 감시역할을 하는
+    리액트의 기술내용을 담고 있는 것이 후크다!
+
+    1. 목적 : 어떤 특정 데이터가 변경될때
+        이 데이터를 할당하여 사용하고 있는 컴포넌트의
+        변경이 반영되도록 하고자 할때 후크를 사용한다!
+
+    2. 구현방법:
+        1) 노드JS SPA 개발환경에서는 상단에 import useState를 한다!
+        -> CDN 에서는 React.useState 로 사용함!
+        2) 코딩법 : useState() 메서드사용
+            배열변수 = useState(초기값)
+            (CDN) -> 배열변수 = React.useState(초기값)
+
+            ((일반형))
+            const [변수명,set변수명] = useState(초기값)
+            -> set변수명 작성시 변수명 첫글자는 대문자로 씀
+            예) 변수명 myname -> setMyname
+            -> set변수명(값) : 메서드형태로 후크변수의 값을 셋팅함!
+
+        3) 작동원리 
+            - useState에 쓴 초기값이 배열변수 첫번째변수에 할당된다!
+            - 코드에서 set변수명에 값을 할당하면
+            useState메서드가 이것을 체크하여 이 변수를 사용한
+            다른부분의 업데이트를 실행한다!
+            예컨데 컴포넌트 내부에 사용한 경우 컴포넌트가 업데이트 됨!
+        4) 사용결과
+            - 별도의 메서드 호출없이 후크 상태변수를 사용한 곳이
+            자동으로 변경될대마다 다시 갱신되는 것을 확인 할 수 있다!
+
+        -> 뷰JS의 리액티브 데이터와 매우 유사함!
+
+*****************************************************************/
