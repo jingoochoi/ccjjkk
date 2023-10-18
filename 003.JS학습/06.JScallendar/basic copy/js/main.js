7a991a817720874bd021a4ff4b7acb4cce36e457
jingoochoi <jingoo0206@gmail.com>
2023-10-18 16:02:24 +0900

    .

--- 003.JS학습/06.JScallendar/basic copy/js/main.js
+++ 003.JS학습/06.JScallendar/basic copy/js/main.js
@@ -3,3 +3,11 @@ import aespa from './dom.js'
 new Call('.dccf1')
 const c2= new Call('.dccf2')
 c2.nxtm()
+const ccdd=$('.calender .main')
+ccdd.mouseenter(function () {
+    // console.log(this)
+    $(this).find('.date').click(()=>{
+        let preval=$(this).next().val()
+        $(this).parents().prev().val(preval)
+    })
+})
