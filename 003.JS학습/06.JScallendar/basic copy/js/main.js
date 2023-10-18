5fe3e6d2b5872d6135d618f3cf09814044a477e7
jingoochoi <jingoo0206@gmail.com>
2023-10-18 16:30:02 +0900

    .

--- 003.JS학습/06.JScallendar/basic copy/js/main.js
+++ 003.JS학습/06.JScallendar/basic copy/js/main.js
@@ -4,6 +4,9 @@ new Call('.dccf1')
 const c2= new Call('.dccf2')
 c2.nxtm()
 const ccdd=$('.calender .main')
+$('input').click(function () {
+    $(this).next().find('.calender').show()
+})
 ccdd.mouseenter(function () {
     // console.log(this)
     $(this).find('.date').click(()=>{
@@ -11,5 +14,6 @@ ccdd.mouseenter(function () {
         preval=preval.split('-')
         preval=preval[0]+'년 '+preval[1]+'월 '+preval[2]+'일'
         $(this).parents().prev().val(preval)
+        $(this).parent().hide()
     })
 })
