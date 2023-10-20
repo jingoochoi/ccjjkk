aeb8f7a11d4136bc8f2be9a3ef51a421a2aa189c
jingoochoi <jingoo0206@gmail.com>
2023-10-20 13:16:16 +0900

    Update 01.공유신발.jsx

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -1,8 +1,11 @@
 // 01.공유신발 JSX
 import myData from './data.js';
 import aespa from './data2.js'
+// console.log(aespa)
+const tdt1=[myData,aespa]
 console.log("%c👞👞","font-size:30px")
 function Main() {
+    const ft=()=>{}
     return(
         <React.Fragment>
             <h1 className="tit">공유가 신고 다닌다는 신발</h1>
@@ -10,6 +13,7 @@ function Main() {
                 <h2>공유는 오늘도 멋져브로</h2>
                 <img src="images/vans/gongyoo.jpg" alt="멋진 공지철" />
             </section>
+            <button onClick={ft} className="bt">효진choice</button>
             <div className="gwrap">
                 <Good/>
             </div>
