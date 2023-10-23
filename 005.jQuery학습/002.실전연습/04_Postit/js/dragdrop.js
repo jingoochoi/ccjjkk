be1ab40874834252689547950bec66bbf83a5219
jingoochoi <jingoo0206@gmail.com>
2023-10-23 12:13:53 +0900

    .

--- 005.jQuery학습/002.실전연습/04_Postit/js/dragdrop.js
+++ 005.jQuery학습/002.실전연습/04_Postit/js/dragdrop.js
@@ -14,6 +14,18 @@ $(()=>{
         drop:function (a,b) {
             $(this).css({backgroundImage:`url(${b.draggable.find('img').attr('src')})`}).html(`🎉${b.draggable.find('p').html()}🎉`)
             b.draggable.hide()
+            $('.tube').html(`
+                <div id="mbox"><a href="#">x</a></div>
+            `)
+            let mbox=$('#mbox')
+            mbox.css({position:'fixed',top:0,left:0,width:'100%',height:'100%',zIndex:3333,backgroundColor:'black'}).find('a').css({position:'absolute',top:0,right:0,fontSize:'100px',textDecoration:'none',color:'white'})
+            mbox.append(`<iframe src="https://www.youtube.com/embed/${b.draggable.attr('data-mv')}?autoplay=1" allow="autoplay" style="border:none;height:100%;width:100%"></iframe>`).hide().delay(1000).slideDown(1000)
+            mbox.find('a').click(function () {
+                mbox.slideUp(1000,function () {
+                    $(this).remove()
+                })
+                b.draggable.show().css({top:0,left:0})
+            })
         }
     })
 })
