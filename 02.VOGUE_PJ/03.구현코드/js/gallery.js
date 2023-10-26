250ed1ef0004d76a31eccba3ff37a6708ddd84d3
jingoochoi <jingoo0206@gmail.com>
2023-10-26 16:42:06 +0900

    Update gallery.js

--- 02.VOGUE_PJ/03.구현코드/js/gallery.js
+++ 02.VOGUE_PJ/03.구현코드/js/gallery.js
@@ -32,5 +32,23 @@ var swiper = new Swiper(".mySwiper", {
     navigation:{
         nextEl:'.swiper-button-next',
         prevEl:'.swiper-button-prev'
-    }
+    },
+    loop:true,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+      }
   });
