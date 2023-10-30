c437fadd8e3a326f516386f31d887e04554f172e
jingoochoi <jingoo0206@gmail.com>
2023-10-30 11:18:07 +0900

    Update cream.js

--- 005.jQuery학습/002.실전연습/05_DragGallery/js/cream.js
+++ 005.jQuery학습/002.실전연습/05_DragGallery/js/cream.js
@@ -2,4 +2,22 @@ $(()=>{
     $('#move').draggable({
         axis:'x'
     }).css({transition:'1s ease-out'})
+    const udtw=()=>{return $(window).width()}
+    let ww=udtw()
+    $(window).resize(()=>{
+        ww=udtw()
+    })
+    if (ww<500) {
+        $('#move').css({transition:'.3s ease-out'})
+    }
+    $('html,body').on('mousedown mouseup mousemove',()=>{
+        
+        if ($('#move').offset().left>0) {
+            $('#move').css({left:0})
+        }
+        if ($('#move').offset().left<-$('#move').width()+ww) {
+            $('#move').css({left:-$('#move').width()+ww})
+            // 
+        }
+    })
 })
