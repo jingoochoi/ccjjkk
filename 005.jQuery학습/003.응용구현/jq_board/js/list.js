79b4e470839c7a8c2dddaaaca4abfab12b831012
jingoochoi <jingoo0206@gmail.com>
2023-10-30 17:18:49 +0900

    Update list.js

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -1,2 +1,23 @@
 // 리스트 페이지 JS - list.js
 
+import sakura from './data.json' assert{type:'json'}
+/* 
+<tr>
+    <td>idx</td>
+    <td>tit</td>
+    <td>writer</td>
+    <td>date</td>
+    <td>cnt</td>
+</tr>
+ */
+// console.log(sakura)
+sakura.sort((a,b)=>b.idx-a.idx)
+$('#board tbody').html(sakura.map(a=>`
+<tr>
+<td>${a.idx}</td>
+<td>${a.tit}</td>
+<td>${a.writer}</td>
+<td>${a.date}</td>
+<td>${a.cnt}</td>
+</tr>
+`).join(''))
