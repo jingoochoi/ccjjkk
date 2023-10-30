4de5ee8eb62f3dd0ff69980de0ee0160b888b7c7
jingoochoi <jingoo0206@gmail.com>
2023-10-30 13:15:56 +0900

    Update car.js

--- 005.jQuery학습/002.실전연습/05_DragGallery/js/car.js
+++ 005.jQuery학습/002.실전연습/05_DragGallery/js/car.js
@@ -6,23 +6,36 @@ $(()=>{
     let drag=0
     let pt=0
     let prtt=0
-    $('.cbx').on('mousemove',e=>{
+    $('.cbx').on('mousemove touchmove',e=>{
+        if (prtt) {
+            return
+        }
+        prtt=1
+        setTimeout(() => {
+            prtt=0
+        }, 100/2);
         if (drag) {
-            let drts=pt-e.pageX<0?0:1
+            let drts=pt-(e.pageX||e.changedTouches[0].pageX)<0?0:1
             // console.log(drts)
             rttc(drts)
+            // $('.cbx').css({cursor:'grabbing'})
         }
+        pt=(e.pageX||e.changedTouches[0].pageX)
     })
-    $('.cbx').on('mousedown',e=>{
+    $('.cbx').on('mousedown touchstart',e=>{
         drag=1
-        pt=e.pageX
+        pt=(e.pageX||e.changedTouches[0].pageX)
+        $('.cbx').css({cursor:'grabbing'})
     })
-    $('.cbx').on('mouseup',e=>{
+    $('.cbx').on('mouseup mouseout touchend',e=>{
         drag=0
+        $('.cbx').css({cursor:'grab'})
         
     })
     let frnb=0
+    const cimg=$('.cbx').find('img')//'img:visible'=visible img,:hidden=hidden img
     const rttc=a=>{
+        cimg.eq(frnb).hide()
         if (a) {
             frnb++
             if (frnb==50) {
@@ -34,5 +47,6 @@ $(()=>{
                 frnb=49
             }
         }
+        cimg.eq(frnb).show()
     }
 })
