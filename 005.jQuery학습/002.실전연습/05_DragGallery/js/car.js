4accb08b5d4f4a36ecc314d27c985173d59daf6b
jingoochoi <jingoo0206@gmail.com>
2023-10-30 12:21:58 +0900

    Update car.js

--- 005.jQuery학습/002.실전연습/05_DragGallery/js/car.js
+++ 005.jQuery학습/002.실전연습/05_DragGallery/js/car.js
@@ -3,4 +3,36 @@ $(()=>{
         $('.cbx').append(`<img src="./360view/country${i+1}.jpg" alt="car">`)
     }
     $('.cbx').find('img').hide().first().show()
+    let drag=0
+    let pt=0
+    let prtt=0
+    $('.cbx').on('mousemove',e=>{
+        if (drag) {
+            let drts=pt-e.pageX<0?0:1
+            // console.log(drts)
+            rttc(drts)
+        }
+    })
+    $('.cbx').on('mousedown',e=>{
+        drag=1
+        pt=e.pageX
+    })
+    $('.cbx').on('mouseup',e=>{
+        drag=0
+        
+    })
+    let frnb=0
+    const rttc=a=>{
+        if (a) {
+            frnb++
+            if (frnb==50) {
+                frnb=0
+            }
+        }else{
+            frnb--
+            if (frnb<0) {
+                frnb=49
+            }
+        }
+    }
 })
