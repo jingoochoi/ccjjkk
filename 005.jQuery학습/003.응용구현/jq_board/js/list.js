faf4903769167d89ba3e25087128c1a843c0e43a
jingoochoi <jingoo0206@gmail.com>
2023-10-31 17:20:08 +0900

    Update list.js

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -12,12 +12,43 @@ import sakura from './data.json' assert{type:'json'}
  */
 // console.log(sakura)
 sakura.sort((a,b)=>b.idx-a.idx)
-$('#board tbody').html(sakura.map(a=>`
-<tr>
-<td>${a.idx}</td>
-<td>${a.tit}</td>
-<td>${a.writer}</td>
-<td>${a.date}</td>
-<td>${a.cnt}</td>
-</tr>
-`).join(''))
+let lstn=0
+const addn=a=>lstn=++a
+// $('#board tbody').html(sakura.map(a=>`
+// <tr>
+// <td>${addn(lstn)}</td>
+// <td>${a.tit}</td>
+// <td>${a.writer}</td>
+// <td>${a.date}</td>
+// <td>${a.cnt}</td>
+// </tr>
+// `).join(''))
+/* 
+paging principle
+1.setting the number of the list per page and the number of the pages
+2.using 'for' as the number
+3.update refer to page number
+ */
+const pgbl=9
+let pgnb=1
+const ttct=sakura.length
+// console.log(ttct)
+let pggg=Math.floor(ttct/pgbl)
+let adov=ttct%pgbl
+// console.log(adov)
+$('#board tbody').html('')
+lstn=(pgnb-1)*pgbl
+for (let i = (pgnb-1)*pgbl; i < pgbl*pgnb; i++) {
+    if (i>=ttct) {
+        break
+    }
+    $('#board tbody').append(`
+    <tr>
+    <td>${addn(lstn)}</td>
+    <td>${sakura[i].tit}</td>
+    <td>${sakura[i].writer}</td>
+    <td>${sakura[i].date}</td>
+    <td>${sakura[i].cnt}</td>
+    </tr>`)
+    
+}
