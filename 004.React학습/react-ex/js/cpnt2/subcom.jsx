d18dfa48670b1f2027612cc9c5b98886ce328490
jingoochoi <jingoo0206@gmail.com>
2023-11-01 17:29:52 +0900

    Update subcom.jsx

--- 004.React학습/react-ex/js/cpnt2/subcom.jsx
+++ 004.React학습/react-ex/js/cpnt2/subcom.jsx
@@ -1,6 +1,20 @@
 import { whom } from "./prvd.jsx";
 function Yagi() {
     const maum=React.useContext(whom)
-    return <div></div>
+    const seld=maum.data.find(a=>{
+        if (a.이름==maum.val) {
+            return true
+        }
+    })
+    return (<div style={{position:'relative',
+    padding:'20px',
+    border:'10px dotted skyblue',
+    borderRadius:'10px',
+    width:'60%',
+    margin:'20px auto',
+    textAlign:'center'}}>
+        <h1>{maum.val}</h1>
+        <img src={seld.이미지} alt={maum.val} style={{width:'100%',opacity:.3}} />
+    </div>)
 }
 export default Yagi
