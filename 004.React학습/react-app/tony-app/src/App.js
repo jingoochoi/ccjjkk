02ee96b1d94cf6d25a24a34654f88e14973f310b
jingoochoi <jingoo0206@gmail.com>
2023-11-08 13:16:43 +0900

    Update App.js

--- 004.React학습/react-app/tony-app/src/App.js
+++ 004.React학습/react-app/tony-app/src/App.js
@@ -1,11 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import {useEffect} from 'react'
+import $ from 'jquery'
+window.jQuery=$
+require ('jquery-ui-dist/jquery-ui')
+require ('jquery-ui-touch-punch/jquery.ui.touch-punch')
 function App() {
+  useEffect(()=>{
+    $('.App-header').click(()=>{
+      window.open('https://www.naver.com/')
+    })
+    $('h1').draggable()
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={'./images/dcm34.jpg'} className="App-logo" alt="logo" />
+        <img src={'./images/dcm34.jpg'} style={{}} className="App-logo" alt="logo" />
+        <h1>자 이제 시작이야 내 꿈을</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
