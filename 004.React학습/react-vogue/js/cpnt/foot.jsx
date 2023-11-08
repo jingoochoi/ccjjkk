b66063d961e5bf0674e7e84ed7e3a81c22055a44
jingoochoi <jingoo0206@gmail.com>
2023-11-08 15:52:25 +0900

    Update foot.jsx

--- 004.React학습/react-vogue/js/cpnt/foot.jsx
+++ 004.React학습/react-vogue/js/cpnt/foot.jsx
@@ -1,39 +1,42 @@
 export default function Foot() {
     return(
-        <React.Fragment>
-            <div id="footarea">
-            <footer class="footarea ibx comarea">
-
-            
-            <div className="blogo">
-                <img src="./images/footer_logo.png" alt="하단로고"/>
-            </div>
-{/* <!-- 3-2.회사주소 --> */}
-            <address className="addr">
-                WWW.VOGUE.COM Ⓒ CONDÉNAST ASIA PACIFIC. INC. 
-                ALL RIGHTS RESERVED. VOGUE.COM KOREA IS OPERATED 
-                BY DOOSAN MAGAZINE.
-            </address>
-{/* <!-- 3-3.하단링크 --> */}
-            <ul className="blink">
-                <li>
-                    <a href="#">정기구독</a>
-                </li>
-                <li>
-                    <a href="#">회사소개</a>
-                </li>
-                <li>
-                    <a href="#">광고 및 제휴</a>
-                </li>
-                <li>
-                    <a href="#">개인정보 처리방침</a>
-                </li>
-            </ul>
-            </footer>
-        </div>
-        <a href="#" class="tbtn fi fi-angle-up">
-        <span class="ir">위로가기버튼</span>
-    </a>
-        </React.Fragment>
+        <Ftmm></Ftmm>
     )
 }
+const Ftmm = React.memo((p) => {
+    return (<React.Fragment>
+        <div id="footarea">
+        <footer class="footarea ibx comarea">
+
+        
+        <div className="blogo">
+            <img src="./images/footer_logo.png" alt="하단로고"/>
+        </div>
+{/* <!-- 3-2.회사주소 --> */}
+        <address className="addr">
+            WWW.VOGUE.COM Ⓒ CONDÉNAST ASIA PACIFIC. INC. 
+            ALL RIGHTS RESERVED. VOGUE.COM KOREA IS OPERATED 
+            BY DOOSAN MAGAZINE.
+        </address>
+{/* <!-- 3-3.하단링크 --> */}
+        <ul className="blink">
+            <li>
+                <a href="#">정기구독</a>
+            </li>
+            <li>
+                <a href="#">회사소개</a>
+            </li>
+            <li>
+                <a href="#">광고 및 제휴</a>
+            </li>
+            <li>
+                <a href="#">개인정보 처리방침</a>
+            </li>
+        </ul>
+        </footer>
+    </div>
+    <a href="#" class="tbtn fi fi-angle-up">
+    <span class="ir">위로가기버튼</span>
+</a>
+    </React.Fragment>)
+});
