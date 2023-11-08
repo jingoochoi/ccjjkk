6d62daac851285502cc4e324ec4876266e649ec6
jingoochoi <jingoo0206@gmail.com>
2023-11-08 11:27:07 +0900

    .

--- 004.React학습/react-app/tony-app/src/App.js
+++ 004.React학습/react-app/tony-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={'https://img.freepik.com/free-vector/planet-earth_1308-82523.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1699315200&semt=sph'} className="App-logo" alt="logo" style={{width:'100px',height:'100px'}} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
