ad44c42b387ac10b636db8a989d62ecce4870c37
jingoochoi <jingoo0206@gmail.com>
2023-11-09 16:34:13 +0900

    .

---
+++ 004.React학습/react-app/tony-app/src/dc/data/img.js
@@ -0,0 +1,14 @@
+export const isrc = {
+    // 각 서브 페이지 이미지경로
+    character:"./images/dcm23.jpg",
+    comics:"./images/dcm24.jpg",
+    games:"./images/dcm26.jpg",
+    movies:"./images/dcm25.jpg",
+    news:"./images/dcm29.jfif",
+    video:"https://www.youtube-nocookie.com/embed/fXEOXVZIQVM?wmode=opaque&controls=&modestbranding=1&rel=0&showinfo=0&color=white&autohide=1&enablejsapi=1&origin=https%3A%2F%2Fwww.dc.com",
+    ost:'https://www.youtube.com/watch?v=-0HH-DH9YA0',
+    // 로고
+    logo: "./images/svg/dc-logo.svg",
+    // 에러페이지
+    error:"https://shop.dc.com/coming-soon/static/media/mxyzptlk.0f810a8a.png"
+};
