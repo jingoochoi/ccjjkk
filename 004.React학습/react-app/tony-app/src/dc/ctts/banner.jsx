1fbaf18adc9e4bec243527ae036e0e901a925499
jingoochoi <jingoo0206@gmail.com>
2023-11-10 12:14:07 +0900

    Update banner.jsx

--- 004.React학습/react-app/tony-app/src/dc/ctts/banner.jsx
+++ 004.React학습/react-app/tony-app/src/dc/ctts/banner.jsx
@@ -4,14 +4,36 @@ import { useEffect } from "react";
 import $ from 'jquery'
 import 'jquery-ui-dist/jquery-ui'
 function sld1() {
+    let cl=0
+    let nb=0
     $('.abtn').click(function () {
+        if (cl) {
+            return
+        }
+        cl=1
+        setTimeout(() => {
+            cl=0
+        }, 1000);
         if ($(this).is('.rb')) {
             $('.slider').animate({left:'-100%'},300,'linear',()=>{
                 $('.slider').append($('.slider').find('li').first()).css({left:0})
             })
+            nb++
+            if (nb>$('.indic li').length-1) {
+                nb=0
+                $('.indic').find('li').first().addClass('on')
+            }
+            $('.indic').find('li').eq(nb).addClass('on').siblings().removeClass('on')
         }
         if ($(this).is('.lb')) {
             $('.slider').prepend($('.slider').find('li').last()).css({left:'-100%'}).animate({left:0},300,'linear')
+            $('.indic').find('li.on').removeClass('on').prev().addClass('on')
+            nb--
+            if (nb<0) {
+                nb=$('.indic li').length-1
+                $('.indic').find('li').last().addClass('on')
+            }
+            $('.indic').find('li').eq(nb).addClass('on').siblings().removeClass('on')
         }
     })
 } 
