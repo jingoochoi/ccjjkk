b17b9dfce718815fe6627f34519ff06d287aea50
jingoochoi <jingoo0206@gmail.com>
2023-11-10 10:20:46 +0900

    Update banner.jsx

--- 004.React학습/react-app/tony-app/src/dc/ctts/banner.jsx
+++ 004.React학습/react-app/tony-app/src/dc/ctts/banner.jsx
@@ -1,6 +1,8 @@
 import { banData } from "../data/bann";
 import '../../css/banner.css'
+import { useEffect } from "react";
 export function Bann(p) {
+    useEffect(()=>{},[])
     const make=(d)=>d.map((a,b)=><li key={b}><img src={a.src} alt="img" /></li>)
     return(
         <div className="banner">
