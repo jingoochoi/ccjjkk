6d1735559555225b9dcd6e5164ae087d75f80c7a
jingoochoi <jingoo0206@gmail.com>
2023-11-22 10:51:15 +0900

    Update App.js

--- 003.dc/03.DC_PJ/02.구현소스/dc-app/src/App.js
+++ 003.dc/03.DC_PJ/02.구현소스/dc-app/src/App.js
@@ -21,10 +21,13 @@ function App() {
     </div>
   );
 }
+// <Link to={주소} state={{속성:값}}></Link>
 // import {useNavigate} from 'react-router-dom'
 // useNavigate(sth)->const에 담아야 한다.->const 안에 링크 주소를 삽입
+// 이름(주소,{state:{속성:값}})
 // *에 링크 삽입->split('*')[0]+<a>tag+split('*')[1]
 // 페이지 이동 시 스크롤 위치 상단으로->useLayoutEffect(()=>{window.scrollTo(0,0)})
 // import {useLocation} from 'react-router-dom'
 // useLocation()-라우터 전달값 받기->const에 담기
+// const 속성이름=이름.state.속성
 export default App;
