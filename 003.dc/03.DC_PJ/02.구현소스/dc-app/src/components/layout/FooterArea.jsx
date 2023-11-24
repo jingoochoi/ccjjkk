a52d41294709a9c4793a49dad7e76f3072bbf586
jingoochoi <jingoo0206@gmail.com>
2023-11-24 14:54:00 +0900

    .

--- 003.dc/03.DC_PJ/02.구현소스/dc-app/src/components/layout/FooterArea.jsx
+++ 003.dc/03.DC_PJ/02.구현소스/dc-app/src/components/layout/FooterArea.jsx
@@ -1,11 +1,22 @@
 // 하단영역 컴포넌트
 
+import { bmenuData } from "../data/bmenu";
+import { Logo } from "../modules/Logo";
+
 export function FooterArea(){
     return(
         <footer className="info">
-            All Site Content © &amp; TM DC, unless otherwise noted here.
-            <br />
-            All rights reserved.
+           <ul>
+            <li><Logo logoStyle="bottom"></Logo></li>
+            <li>
+                <ol className="bmenu">
+                    {bmenuData.map((a,b)=>
+                        <li key={b}><a href={a.link} target="_blank">{a.txt.toUpperCase()}</a></li>
+                    )}
+                </ol>
+            </li>
+            <li>© & ™ DC. ALL RIGHTS RESERVED</li>
+           </ul>
         </footer>
     )
 
