10cb824b3fc73dc4c34bf70810d8dad5dac24377
jingoochoi <jingoo0206@gmail.com>
2023-11-24 15:12:17 +0900

    Update swps.jsx

--- 004.de/04.DE_PJ/02.구현소스/pilot-app/src/plugin/swps.jsx
+++ 004.de/04.DE_PJ/02.구현소스/pilot-app/src/plugin/swps.jsx
@@ -9,16 +9,16 @@ import { Pagination,Navigation,Keyboard,Autoplay } from 'swiper/modules';
 import './css/swps.css';
 export function Swps(p) {
     const mymy=useRef(null)
-    const next=()=>{
-        mymy.current.swiper.slideNext()
+    const make=(nb)=>{
+        let temp=[]
+        for (let x = 0; x < nb; x++) {
+            temp[x]=
+            <SwiperSlide><img src={"./images/sub/"+p.cat+"/banner/ban"+(x+1)+".png"} alt="men banner" /></SwiperSlide>
+        }
+        return temp
     }
-    let mygo='swiper'
-    let mydo=useRef('gallery')
-    const ft=()=>{
-        mygo='스와이퍼'
-        mydo.current='갤러리'
-        // console.log(mygo,mydo)
-    }
+    
+
     return(
         <>
             
@@ -41,14 +41,12 @@ export function Swps(p) {
             >
                 
                 
-                <SwiperSlide><img src={"./images/sub/"+p.cat+"/banner/ban1.png"} alt="men banner" /></SwiperSlide>
-                <SwiperSlide><img src={"./images/sub/"+p.cat+"/banner/ban2.png"} alt="men banner" /></SwiperSlide>
-                <SwiperSlide><img src={"./images/sub/"+p.cat+"/banner/ban3.png"} alt="men banner" /></SwiperSlide>
+                {make(p.cat=='style'?5:3)}
+                
                 
                 
             </Swiper>
-            <button className='stop' style={{cursor:'pointer'}} onClick={next}>o</button>
-            <button onClick={ft}>testo</button>
+            
         </>
     )
 }
