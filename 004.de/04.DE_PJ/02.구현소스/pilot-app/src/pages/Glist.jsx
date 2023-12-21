cb2a9e071803cc1654d581ce124b1642aa71273c
jingoochoi <jingoo0206@gmail.com>
2023-12-21 16:02:48 +0900

    .

--- 004.de/04.DE_PJ/02.구현소스/pilot-app/src/pages/Glist.jsx
+++ 004.de/04.DE_PJ/02.구현소스/pilot-app/src/pages/Glist.jsx
@@ -1,13 +1,20 @@
+import { useRef, useState } from 'react';
 import '../css/glist.css'
 import gdata from '../data/glist';
+import { Item } from '../modules/Item';
+import $ from 'jquery'
 export function Glist() {
+    const etem=useRef('m1')
+    const cats=useRef('men')
+    const[rand,setRand]=useState(0)
+    // const[tail,setTail]=useState(false)
     function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     const make=()=>
         gdata.map((a,b)=>
-        <div key={b}>
-        <a href="#" >
+        <div key={b} onClick={()=>showtail(a.ginfo[0],a.cat)}>
+        <a href="#" onClick={(e)=>e.preventDefault()}>
           [{b+1}]
           <img src={"./images/goods/"+a.cat+"/"+a.ginfo[0]+".png"} alt="dress" />{" "}
           <aside>
@@ -16,7 +23,12 @@ export function Glist() {
         </a>
       </div>
         )
-    
+    const showtail=(a,b)=>{
+        etem.current=a
+        cats.current=b
+        setRand(Math.random())
+        $('#bgbx').slideDown(300)
+    }
   return (
     <>
       <main id="cont">
@@ -34,6 +46,10 @@ export function Glist() {
             {make()}
           </div>
         </section>
+        {
+            
+        <div id="bgbx" style={{position:'fixed',top:0,paddingTop:'20vh',height:'80vh',backdropFilter:'blur(3px)',zIndex:333333}}><Item cat={cats.current} good={etem.current}></Item></div>
+        }
       </main>
     </>
   );
